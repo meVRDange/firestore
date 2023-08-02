c377e68774bcf98dfefc8d5aaa6e36ee2b1883f7
Vaibhav Dange <95734265+meVRDange@users.noreply.github.com>
2023-08-02 04:14:29 +0000

    added user add form

--- src/App.js
+++ src/App.js
@@ -5,20 +5,43 @@ import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
   const [books, setBooks] = useState([]);
-  const booksRef = collection(db,"Books");
 
-  useEffect(() =>{
+  const [name, setName] = useState('');
+
+  const booksRef = collection(db, "Books");
+
+  const handleName = (e) => {
+    e.preventDefault()
+    var value = e.target.value;
+    console.log(value)
+    // if (value !== '') {
+    //   setName(value);
+    // }
+  }
+
+  useEffect(() => {
     const getBooks = async () => {
       const data = await getDocs(booksRef)
-      setBooks(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      setBooks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
       console.log(books);
     }
     getBooks()
   }, [])
+
   return (
     <>
+      <form onSubmit={handleName}>
+        <input type='text'/>
+        <button type='submit'>submit</button>
+      </form>
       <h1 >Firestore</h1>
-      {}
+      {books.map((book) => {
+        return (
+          <div key={book.id}>
+            <h2>{book.Name}</h2>
+          </div>
+        )
+      })}
     </>
   );
 }
